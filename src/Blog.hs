dab1c4ed3aafbeacdaef983d286f7edd968ff7b8
Athan Clark <athan.clark@gmail.com>
2015-01-26 14:06:36 -0700

    trying to fix directory

--- src/Blog.hs
+++ src/Blog.hs
@@ -37,8 +37,9 @@ handleBlogPosts mainHtml = do
     postTitle = T.pack . inlineToString . docTitle
 
   pr <- envPrefix <$> lift ask
+  -- liftIO $ print $ pr <> "blog/"
   postFiles <- (filter (\x -> x /= "." && x /= "..")) <$>
-                  (liftIO $ getDirectoryContents $ pr <> "blog/")
+                  (liftIO $ getDirectoryContents $ pr <> "blog")
   postMetas <- mapM (\f -> postFileMeta $ pr <> "blog/" <> f) postFiles
 
   let
